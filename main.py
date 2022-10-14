da4b60679bbe3a223a43f80badd070ad88165548
Nihal Siddiqui <NihalSiddique355@gmail.comm>
2022-10-14 22:43:35 +0530

    update

---
+++ main.py
@@ -0,0 +1,19 @@
+# Note First install AIML Module
+# go to command pro :-     pip install python-aiml
+
+import aiml
+
+kernel = aiml.Kernel()
+kernel.learn("std-startup.xml")
+kernel.respond("load aiml b")
+
+while True:
+  input_text = input(">Human:  ")
+  response = kernel.respond(input_text)
+  print("Bot> " + response)
+
+
+
+
+
+
